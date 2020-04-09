49e4707d673c1ee0341a81fe36643e284ee2c7b1
allisoncortez <allizetorc@gmail.com>
2020-04-09 18:30:50 -0500

    added has_secure_password to User model, updates to Gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,8 @@ class User < ApplicationRecord
     has_many :comments
     has_many :comments_on_art, through: :comments, source: art_piece
 
+    has_secure_password
+    #authenticate, validate password/password confirmation
+
     
 end
