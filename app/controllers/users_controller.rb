681428c6f0ca622f545ac680b0a8465c7189eadf
allisoncortez <allizetorc@gmail.com>
2020-04-09 21:50:01 -0500

    added views/users for new and show; make a Sign Up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            #log them in
+            session[:user_id] = @user.id
+            #redirect to showpage
+            redirect_to 
+        else
+            render :new
+        end
+    end
+
+    def show
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
 end
