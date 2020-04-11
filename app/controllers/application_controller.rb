ffec976ee1a1e516f072d5241ac90409049e9d2b
allisoncortez <allizetorc@gmail.com>
2020-04-10 20:50:19 -0500

    added new and create to Art controller, added challenge and art views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
     end
 
     def logged_in?
-        !!@current_user
+        !!session[:user_id]
 
         # or we could do 
         #     !!session[:user_id]
--- app/controllers/art_pieces_controller.rb
+++ app/controllers/art_pieces_controller.rb
@@ -1,2 +1,21 @@
 class ArtPiecesController < ApplicationController
+    before_action :redirect_if_not_logged_in
+    def new
+        @art_piece =ArtPiece.new 
+    end
+
+    def create
+        @art_piece = current_user.art_pieces.build(art_params)
+        if @art_piece.save
+            redirect_to art_pieces_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def art_params
+        params.require(:art_piece).permit(:title, :social_handle)
+    end
 end
