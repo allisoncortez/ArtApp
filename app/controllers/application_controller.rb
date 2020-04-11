4a127b9e95a493bf226b40e367df0f4140d09b9e
allisoncortez <allizetorc@gmail.com>
2020-04-10 19:24:21 -0500

    added helper methods to app controller, added links to layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+    # gives access to theses methods in the views
+    helper_method :current_user, :logged_in?
+
+    private
+    def current_user
+        @current_user ||= User.find_by_id{session[:user_id]} if session[:user_id]
+    end
+
+    def logged_in?
+        !!@current_user
+
+        # or we could do 
+        #     !!session[:user_id]
+    end
+
+    def redirect_if_not_logged_in
+        redirect_to '/' if !logged_in?
+    end
+
+
 end
