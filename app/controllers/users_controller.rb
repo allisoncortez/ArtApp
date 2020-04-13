c3e8c8cce154a2a5f4b8cb6da934ac26926eb4bd
allisoncortez <allizetorc@gmail.com>
2020-04-13 15:56:26 -0500

    updated users table to include a first_name attribute

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:email, :password)
+        params.require(:user).permit(:email, :first_name, :password)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_12_152836) do
+ActiveRecord::Schema.define(version: 2020_04_13_204858) do
 
   create_table "art_works", force: :cascade do |t|
     t.string "title"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2020_04_12_152836) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
   end
 
   add_foreign_key "art_works", "challenges"
