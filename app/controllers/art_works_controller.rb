a7625d2da8cc4a4e1fd6a3cc7e702e3259613ce6
allisoncortez <allizetorc@gmail.com>
2020-04-17 16:20:54 -0500

    changed logic in artwork index to serve as my submissions and artist page view

--- app/controllers/art_works_controller.rb
+++ app/controllers/art_works_controller.rb
@@ -3,13 +3,18 @@ class ArtWorksController < ApplicationController
     before_action :set_art_work, except: [:index, :new, :create]
 
     def index
-        if params[:user_id] && current_user = User.find_by_id(params[:user_id])
-            @art_works = current_user.art_works
-        else
+        
+        @user = User.find_by_id(params[:user_id])
+        # @art_works = current_user.art_works
+        @art_works = @user.art_works
+            
+        # else
             # flash[:message] = "Oops! That art doesn't exist." ..we would need to redirect to something, if we use flash here..
-            @error = "Oops! That user doesn't exist." if params[:user_id]
-            @art_works = ArtWork.all 
-        end
+            # @error = "Oops! That user doesn't exist." if params[:user_id]
+            # @art_works = ArtWork.all 
+            # @user = @art_work.user
+
+        # end
     end
 
     def new
