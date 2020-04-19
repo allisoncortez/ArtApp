fa259755be48bab0f869e5d9715c644a72a87120
allisoncortez <allizetorc@gmail.com>
2020-04-19 00:58:12 -0500

    refactoring errors, made application helper method, updated spec checklist

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+    def has_errors?(this_obj)
+        if this_obj.errors
+           render partial: "layouts/errors", locals: {object: this_obj}
+        end
+    end
 end
