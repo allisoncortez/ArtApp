c0d22f030b8bc3190c275113e0c4f0a2e06d6266
allisoncortez <allizetorc@gmail.com>
2020-04-21 12:32:14 -0500

    deleting commented out code, extra code in artworks controller

--- app/controllers/art_works_controller.rb
+++ app/controllers/art_works_controller.rb
@@ -3,70 +3,46 @@ class ArtWorksController < ApplicationController
     before_action :set_art_work, except: [:index, :new, :create]
 
     def index
-        
         @user = User.find_by_id(params[:user_id])
-        # @art_works = current_user.art_works
         @art_works = @user.art_works
             
     end
 
     def new
         if params[:challenge_id] && @challenge = Challenge.find_by_id(params[:challenge_id])
-
             @art_work = @challenge.art_works.build
         else
           @art_work = ArtWork.new  
-        # #     flash[:message] = "Oops, you're not logged in."
-        # #     # redirect_to challenge_art_works_path(@art_work)
-        # #     redirect_to challenge_path(@challenge)
-            @art_work = ArtWork.new  
+
         end
     end
 
-    # def new
-    #     # if params[:challenge_id] && @challenge = Challenge.find_by_id(params[:challenge_id])
-    #     if @challenge = Challenge.find_by_id(params[:challenge_id])
-    #         @art_work = @challenge.art_works.build
-    #     else
-    #     # #     flash[:message] = "Oops, you're not logged in."
-    #     # #     # redirect_to challenge_art_works_path(@art_work)
-    #     # #     redirect_to challenge_path(@challenge)
-    #         @art_work = ArtWork.new  
-    #     end
-    # end
-
-    
     def create
         @art_work = current_user.art_works.build(art_work_params)
 
         if @art_work.save
-            redirect_to art_work_path(@art_work), notice: "Artwork uploaded successfully"
-            # artwork index path: challenge_art_works_path(@art_work)
+            redirect_to art_work_path(@art_work)
+            # redirect_to challenge_art_work_path(@art_work.challenge, @art_work)
         else
             render :new
         end
     end
 
     def show
-        #does the artwork exist
-        #does it have matching ids
-        # @art_work = ArtWork.find_by(id: params[:id])
         
     end 
 
     def edit
-        #   @art_work = ArtWork.find_by(id: params[:id])
+  
           @challenge = @art_work.challenge
         if @art_work.user_id != current_user.id 
             flash[:message] = "Oops, you can't edit this."
-            # redirect_to challenge_art_works_path(@art_work)
             redirect_to challenge_path(@challenge)
         end
     end
 
     def update
-        # @art_work = ArtWork.find_by(id: params[:id])
-
+    
         if @art_work.update(art_work_params)
             redirect_to art_work_path(@art_work)
         else 
@@ -75,13 +51,12 @@ class ArtWorksController < ApplicationController
     end
 
     def destroy
-        # @art_work = ArtWork.find_by(id: params[:id])
+  
         @challenge = @art_work.challenge
        
         @art_work.destroy
         redirect_to challenge_path(@challenge)
-        
-        # redirect_to challenge_art_works_path(@art_work.challenge)
+  
     end
 
     private
